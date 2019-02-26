4b72360d6c62c007c99a2d4d33b26d1770333985
king <king@itheima.com>
2019-02-26 09:13:47 +0800

    本地所有文件第一次git commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a3/f0f49b381e38001918f7dd17be565a3d
@@ -0,0 +1,5 @@
+package com.itheima.demo.service;
+
+public interface UserService {
+
+}
---
+++ dubboxdemo-service/src/main/java/com/itheima/demo/service/UserService.java
@@ -0,0 +1,7 @@
+package com.itheima.demo.service;
+
+public interface UserService {
+
+	public String getName();
+	
+}
